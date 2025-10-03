98f81dffe1932e31b7b0f59dff55424f09982c34
Jessica <jpanndora@gmail.com>
2025-10-03 20:49:39 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double Altura, Peso, imc;
+Console.WriteLine("Cálculo do Índice de Massa Corporal (IMC)");
+Console.Write("Altura (m)..: ");
+Altura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Peso (kg)...: ");
+Peso = Convert.ToDouble(Console.ReadLine());
+imc = Peso / Math.Pow(Altura, 2);
+Console.WriteLine($"\nIMC: {imc:N1} kg/m²");
